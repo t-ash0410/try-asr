9eeaa6d02dc95a1344c21ee1403137a3ffcf63e3
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-03-23 21:38:54 +0900

    fix webtransport

--- go/app/internal/handlers/webtransport.go
+++ go/app/internal/handlers/webtransport.go
@@ -6,11 +6,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"tryhttp3/app/internal/speeachtotext"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	"github.com/quic-go/webtransport-go"
-
-	"tryhttp3/app/internal/speachtotext"
 )
 
 func HandleRecording(s *webtransport.Server, spc *speech.Client) http.HandlerFunc {
@@ -24,20 +23,34 @@ func HandleRecording(s *webtransport.Server, spc *speech.Client) http.HandlerFun
 			return
 		}
 
-		s, err := speachtotext.SpeechToText(ctx, spc, func() (io.Reader, error) {
-			stream, err := conn.AcceptUniStream(ctx)
-			if err != nil {
-				log.Printf("Failed to accept stream: %s\n", err)
-				return nil, io.EOF
-			}
-			return stream, nil
-		})
+		stream, err := speeachtotext.NewStream(r.Context(), spc)
 		if err != nil {
-			log.Printf("Failed to speech to text: %s\n", err)
-			w.WriteHeader(500)
+			fmt.Println("Failed to create stream:", err)
 			return
 		}
 
+		for {
+			reader, err := conn.AcceptUniStream(ctx)
+			if err != nil {
+				log.Printf("Failed to accept stream: %s\n", err)
+				break
+			}
+			audio, err := io.ReadAll(reader)
+			if err != nil {
+				log.Printf("Failed to read audio: %s\n", err)
+				return
+			}
+			if err := speeachtotext.SendAudio(stream, audio); err != nil {
+				fmt.Println("Failed to send audio:", err)
+				return
+			}
+		}
+
+		s, err := speeachtotext.Result(stream)
+		if err != nil {
+			fmt.Println("Failed to get result:", err)
+			return
+		}
 		fmt.Println("Result:", s)
 	}
 }
